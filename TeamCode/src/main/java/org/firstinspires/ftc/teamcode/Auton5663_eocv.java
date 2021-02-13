d92afb5adff58a5b77de0def93741134c3252fa0
paulgobble <paul@paulgobble.com>
2021-02-13 16:34:08 -0500

    Unifying autonomous op modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Auton5663_eocv", group="Autonomous")
@@ -14,6 +15,16 @@ public class Auton5663_eocv extends LinearOpMode {
     Robot robot   = new Robot();
     private ElapsedTime runtime = new ElapsedTime();
     private ElapsedTime segmentTime = new ElapsedTime();
+
+    //Set up variables for erncoders to wheel distance.
+    static final double     COUNTS_PER_MOTOR_REV    = 383.6 ;  //GoBilda Motor 28 counts per motor rev (28*13.7=383.6)
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;
+    static final double     WHEEL_DIAMETER_INCHES   = 3.93734 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     LOW_SPEED             = 0.25;
+    static final double     HIGH_SPEED              = 0.4;
+
 
     @Override
     public void runOpMode() {
@@ -31,13 +42,45 @@ public class Auton5663_eocv extends LinearOpMode {
 
         robot.hMap(hardwareMap);
 
+        // paste code here
+        //Tell the driver that the encoders are resetting.
+        telemetry.addData("Status:", "Initialized v1.9.8");
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        // Reverse the motor that runs backwards when connected directly to the battery
+        //robot.FRDrive.setDirection(DcMotor.Direction.FORWARD);
+        //robot.FLDrive.setDirection(DcMotor.Direction.REVERSE);
+        //robot.BRDrive.setDirection(DcMotor.Direction.FORWARD);
+        //robot.BLDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        //Reset all encoders to have a fresh start when the match starts.
+        //Drive
+        robot.FLDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.FRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.BLDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.BRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Turn off RUN_TO_POSITION.
+        //Drive
+        robot.FLDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.FRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.BLDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.BRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("FLDrive:",robot.FLDrive.getCurrentPosition());
+        telemetry.addData("FRDrive:", robot.FRDrive.getCurrentPosition());
+        telemetry.addData("BLDrive:", robot.BLDrive.getCurrentPosition());
+        telemetry.addData("BRDrive:", robot.BRDrive.getCurrentPosition());
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         
         runtime.reset();
 
-        watchAndReport(30);
+        watchAndReport(3);
+
+        prepChickenWing(10);
 
     } // end runOpMode
 
@@ -60,5 +103,7 @@ public class Auton5663_eocv extends LinearOpMode {
             }
         }
     } // end watchAndReport
+
+
 
 }  // end class Auton5663_eocv
