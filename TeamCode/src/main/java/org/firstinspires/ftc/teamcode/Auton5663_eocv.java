b5cfef98d03e85eaecf04627c5cd2a7b1daa5462
dsgamer1717 <dylanbudda17@yahoo.com>
2021-03-06 16:15:13 -0500

    WIP Auton.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
@@ -347,11 +347,11 @@ public class Auton5663_eocv extends LinearOpMode {
 
                 if(robot.LaChickenWing.getCurrentPosition() < desiredWingPosition)
                 {
-                    robot.LaChickenWing.setPower(0.35);
+                   // robot.LaChickenWing.setPower(0.35);
                 }
                 else
                 {
-                    robot.LaChickenWing.setPower(0);
+                   // robot.LaChickenWing.setPower(0);
                 }
             }
 
