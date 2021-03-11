6e8f8ba26689351b8fea4b854cb72f7507842bae
dsgamer1717 <dylanbudda17@yahoo.com>
2021-03-11 16:45:16 -0500

    WIP Auton. Added rotate left/right to get the launcher in position and then I make it shoot the rings.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
@@ -95,6 +95,17 @@ public class Auton5663_eocv extends LinearOpMode {
         // Stage 02
         ShootForPowerShots(2.5); // was 3.5
 
+        rotateLeftToShoot(1);
+
+        ShootForPowerShots(2.5);
+
+        rotateRightToShoot(1);
+
+        rotateRightToShoot(1);
+
+        ShootForPowerShots(2.5);
+
+        rotateLeftToShoot(1);
         // Stage 03.1
         MoveToWhiteLineForDecision(3);  // was 5
 
@@ -412,6 +423,7 @@ public class Auton5663_eocv extends LinearOpMode {
             segmentTime.reset();
 
             // Method Set up code goes here
+            boolean canFire = true;
 
             // Telemetry
             telemetry.addData("Stage No", "02");
@@ -419,11 +431,13 @@ public class Auton5663_eocv extends LinearOpMode {
             telemetry.update();
 
             // Check if its safe to run this method
-            while (opModeIsActive() && (segmentTime.seconds() < segmentTimeLimit)) {
+            while (opModeIsActive() && (segmentTime.seconds() < segmentTimeLimit && canFire)) {
 
                 robot.Shooter(0.85);
                 sleep(1000);
                 robot.Intake.setPower(0.9);
+                sleep(1000);
+                canFire = false;
 
                 // update time telemetry readout
                 telemetry.addData("Runtime", "%.3f", runtime.seconds());
@@ -436,6 +450,55 @@ public class Auton5663_eocv extends LinearOpMode {
         }
     } // end ShootForPowerShots
 
+    public void rotateRightToShoot(double segmentTimeLimit) {
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+            // reset the segment timer
+            segmentTime.reset();
+
+            // Drive Targets
+            double speed = .5;
+            double FL_Distance = 5;
+            double FR_distance = -5;
+            double BL_distance = 5;
+            double BR_distance = -5;
+
+            // Telemetry
+            telemetry.addData("Stage No", "999");
+            telemetry.addData("Stage Desc", "Drive stuff");
+            telemetry.update();
+
+            // call encoderDrive
+            encoderDrive(speed, FL_Distance, FR_distance, BL_distance, BR_distance, segmentTimeLimit);
+
+        }
+    } // end rotateRightToShoot
+
+    public void rotateLeftToShoot(double segmentTimeLimit) {
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+            // reset the segment timer
+            segmentTime.reset();
+
+            // Drive Targets
+            double speed = .5;
+            double FL_Distance = -5;
+            double FR_distance = 5;
+            double BL_distance = -5;
+            double BR_distance = 5;
+
+            // Telemetry
+            telemetry.addData("Stage No", "999");
+            telemetry.addData("Stage Desc", "Drive stuff");
+            telemetry.update();
+
+            // call encoderDrive
+            encoderDrive(speed, FL_Distance, FR_distance, BL_distance, BR_distance, segmentTimeLimit);
+
+        }
+    } // end rotateLeftToShoot
 
     // SCRIPT STAGE 03.1
     // Drive Segment
