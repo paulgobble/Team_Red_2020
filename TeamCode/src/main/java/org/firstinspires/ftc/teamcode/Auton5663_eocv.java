a1557ddce192d452d7417f48feb0472d74852278
dsgamer1717 <dylanbudda17@yahoo.com>
2021-03-11 17:25:38 -0500

    WIP Auton.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
@@ -436,7 +436,7 @@ public class Auton5663_eocv extends LinearOpMode {
             // Check if its safe to run this method
             while (opModeIsActive() && (segmentTime.seconds() < segmentTimeLimit) && canFire ) {
 
-                robot.Shooter(0.85);
+                robot.Shooter(0.7);
                 sleep(1000);
                 robot.Intake.setPower(0.9);
                 encoderDrive(speed, FL_Distance2, FR_distance2, BL_distance2, BR_distance2, segmentTimeLimit);
