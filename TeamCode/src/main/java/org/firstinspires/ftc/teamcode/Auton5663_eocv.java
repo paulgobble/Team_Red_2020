025d8da824eb7f1fb3b3a4d2dba628002dbaf1f4
dsgamer1717 <dylanbudda17@yahoo.com>
2021-03-12 19:29:13 -0500

    WIP Auton5663_eocv.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton5663_eocv.java
@@ -1010,10 +1010,10 @@ public class Auton5663_eocv extends LinearOpMode {
 
             // Drive Targets
             double speed = .3; // was .5
-            double FL_Distance = -31; // was 30
-            double FR_distance = -30;
-            double BL_distance = -30;
-            double BR_distance = -30;
+            double FL_Distance = -27.5; // was 30
+            double FR_distance = -27.5;
+            double BL_distance = -27.5;
+            double BR_distance = -27.5;
 
             // Telemetry
             telemetry.addData("Stage No", "13_C");
